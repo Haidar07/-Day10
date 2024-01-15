81ab41d1e386fdb3f79dfde7b25e3fd5f182aac9
Haidar07 <haidarjf07@gmail.com>
2024-01-15 21:30:15 +0700

    Task 11 - pembuatan routing halaman dan percobaan penerapan params

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,9 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!')
-})
+app.set('view engine', 'hbs')
+app.set('views', 'src/views')
+
+app.use(`/assets`, express.static(`src/assets`));
+
+app.get('/', home);
+app.get('/addProject', addProject);
+app.get('/detilProject/:id', detilProject);
+app.get('/contactMe', contactMe);
+app.get('/testi-OOP', testiOOP);
+app.get('/testimonial_HoF', testiHoF);
+app.get('/testimonial_JSON', testiJson);
+
+function home(req, res) {res.render(`index`);}
+function addProject(req, res) {res.render(`addProject`);}
+function detilProject(req, res) {
+  const {id} = req.params;
+  console.log(id);
+
+  res.render(`detilProject`, { id });
+}
+function contactMe(req, res) {res.render(`contactMe`);}
+function testiOOP(req, res) {res.render(`testi-OOP`);}
+function testiHoF(req, res) {res.render(`testimonial_HoF`);}
+function testiJson(req, res) {res.render(`testimonial_JSON`);}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
